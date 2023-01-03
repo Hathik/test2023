f9a18f2ab174b28ca4cf2c065b53a0df3f2a2897
hathik <mh.hathik22@gmail.com>
2023-01-03 22:33:55 +0530

    c has added

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ class sum
     public static void main(String args[]){
         int a = 3;
         int b = 8;
+        int c = 6;
         sum = a+b;
         System.out.println(a+b); //summation
         System.out.println(sum);
