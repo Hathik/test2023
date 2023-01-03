d66ae4658a6ed8484a207c9b736c60d334e17352
M.H.Haathik Ahamed <107987176+Hathik@users.noreply.github.com>
2023-01-03 14:05:10 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,8 @@ class sum
     public static void main(String args[]){
         int a = 3;
         int b = 8;
+        sum = a+b;
         System.out.println(a+b); //summation
+        System.out.println(sum);
     }
 }
